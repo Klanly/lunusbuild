6ccc552017d41b6821540b90fada910ca29a0358
Klanly <natural858@gmail.com>
2020-11-01 22:09:11 +0800

    Create ytcodelist.py

---
+++ ytcodelist.py
@@ -0,0 +1,20 @@
+
+epidict24={
+    
+    20:['oLVt6JwSy1Y'],
+
+    32:['TT5LUXS1Z8I','FaJ2j5MklEM'],
+
+    56:['ogvJpzhcF1g']
+    
+    }
+
+
+epidict60={
+    
+    20:['Y2bXYiwSydk','oMb6DMj7yys'],
+    
+    32:['qyGTjSE62ok','rSkp1K5cMQM','al_vY8ss174','6RH95TTz59M'],
+
+    56:['pkgL8SfwKew','EALmMQhXilI']
+           }
