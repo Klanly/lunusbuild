832c6991b87b8c1afaab29f76681b0fb598cfef9
Klanly <natural858@gmail.com>
2020-11-02 09:44:24 +0800

    Update ytcodelist.py

--- ytcodelist.py
+++ ytcodelist.py
@@ -3,7 +3,7 @@ epidict24={
     
     20:['oLVt6JwSy1Y'],
 
-    32:['TT5LUXS1Z8I','FaJ2j5MklEM'],
+    32:['LYEQD4s_5aA','TT5LUXS1Z8I','FaJ2j5MklEM'],
 
     56:['ogvJpzhcF1g']
     
@@ -14,7 +14,7 @@ epidict60={
     
     20:['Y2bXYiwSydk','oMb6DMj7yys'],
     
-    32:['qyGTjSE62ok','rSkp1K5cMQM','al_vY8ss174','6RH95TTz59M'],
+    32:['xQOmuZC93wk','4pzBk2HNbt4','qyGTjSE62ok','rSkp1K5cMQM','al_vY8ss174','6RH95TTz59M'],
 
     56:['pkgL8SfwKew','EALmMQhXilI']
            }
