386a18318b45dc69083174d7bc48f51d7c6bb153
Klanly <natural858@gmail.com>
2020-11-04 10:46:03 +0800

    Update ytcodelist.py

--- ytcodelist.py
+++ ytcodelist.py
@@ -3,7 +3,7 @@ epidict24={
     
     20:['oLVt6JwSy1Y'],
 
-    32:['LYEQD4s_5aA','TT5LUXS1Z8I','FaJ2j5MklEM'],
+    32:['rDR6sO7KVdQ','LYEQD4s_5aA','TT5LUXS1Z8I','FaJ2j5MklEM'],
 
     56:['ogvJpzhcF1g']
     
@@ -14,7 +14,7 @@ epidict60={
     
     20:['Y2bXYiwSydk','oMb6DMj7yys'],
     
-    32:['xQOmuZC93wk','4pzBk2HNbt4','qyGTjSE62ok','rSkp1K5cMQM','al_vY8ss174','6RH95TTz59M'],
+    32:['5gCqHkY_h7U','l13DQBdqeeM','xQOmuZC93wk','4pzBk2HNbt4','qyGTjSE62ok','rSkp1K5cMQM','al_vY8ss174','6RH95TTz59M'],
 
     56:['pkgL8SfwKew','EALmMQhXilI']
            }
