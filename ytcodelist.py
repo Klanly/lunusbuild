e5c11a14b069fc7b9ad2492430f2db6fbfb86376
Klanly <natural858@gmail.com>
2020-11-09 17:42:45 +0800

    Update ytcodelist.py

--- ytcodelist.py
+++ ytcodelist.py
@@ -3,7 +3,7 @@ epidict24={
     
     20:['oLVt6JwSy1Y'],
 
-    32:['3tc82tWyFkU','rDR6sO7KVdQ','LYEQD4s_5aA','TT5LUXS1Z8I','FaJ2j5MklEM'],
+    32:['BaI6w2eNZ0I','3tc82tWyFkU','rDR6sO7KVdQ','LYEQD4s_5aA','TT5LUXS1Z8I','FaJ2j5MklEM'],
 
     56:['ogvJpzhcF1g']
     
@@ -14,7 +14,7 @@ epidict60={
     
     20:['Y2bXYiwSydk','oMb6DMj7yys'],
     
-    32:['xBBmkdVGxUY','TJ6gZ7nOaoM','5gCqHkY_h7U','l13DQBdqeeM','xQOmuZC93wk','4pzBk2HNbt4','qyGTjSE62ok','rSkp1K5cMQM','al_vY8ss174','6RH95TTz59M'],
+    32:['d5kWIfuxYtY','gG0tKw87rvE','xBBmkdVGxUY','TJ6gZ7nOaoM','5gCqHkY_h7U','l13DQBdqeeM','xQOmuZC93wk','4pzBk2HNbt4','qyGTjSE62ok','rSkp1K5cMQM','al_vY8ss174','6RH95TTz59M'],
 
     56:['pkgL8SfwKew','EALmMQhXilI']
            }
